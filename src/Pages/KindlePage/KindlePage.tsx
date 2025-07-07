2798b2642ea8f6385d794f20a61fd9701cd9654c
anastasiia <anastasiia.zuieva4@gmail.com>
2025-07-07 20:06:36 +0300

    anastasiia ui 2 ticket - search bar added

--- src/Pages/KindlePage/KindlePage.tsx
+++ src/Pages/KindlePage/KindlePage.tsx
@@ -1,1 +1,11 @@
-export const KindlePage = () => <h1>KINDLE PAGE</h1>;
+// import React from "react";
+import { SearchBar } from '@/components/ui/input/SearchBar';
+
+export const KindlePage = () => {
+  return (
+    <div className="bg-white">
+      <h1>KINDLE PAGE</h1>
+      <SearchBar />
+    </div>
+  );
+};
