a461145c45d87b7bc66e627fcf4540a877ec517e
lidiia-tsymborovych <lidacimbirovic@gmail.com>
2025-07-07 19:33:49 +0300

    feat: add reusable dropdown select

--- src/Pages/AudiobookPage/AudiobookPage.tsx
+++ src/Pages/AudiobookPage/AudiobookPage.tsx
@@ -1,1 +1,45 @@
-export const AudiobookPage = () => <h1>AUDIOBOOK PAGE</h1>;
+import { DropdownSelect } from '@/components/ui/DropdownSelect';
+
+export const AudiobookPage = () => {
+  return (
+    <>
+      <h1 className="btn-text">AUDIOBOOK PAGE</h1>
+      <br />
+      <DropdownSelect
+        options={[
+          { label: 'Books', value: 'books' },
+          { label: 'Audio Books', value: 'audio' },
+          { label: 'E-books', value: 'ebooks' },
+        ]}
+        placeholder="Choose category"
+        className="w-full sm:w-[176px]"
+      />
+
+      <br />
+
+      <DropdownSelect
+        label="Sort by"
+        options={[
+          { label: 'Newest', value: 'newest' },
+          { label: 'Price: Low to High', value: 'low-high' },
+          { label: 'Price: High to Low', value: 'high-low' },
+        ]}
+        defaultValue="newest"
+        className="w-[136px] sm:w-[186px] xl:w-[176px]"
+      />
+
+      <br />
+
+      <DropdownSelect
+        label="Items per page"
+        options={[
+          { label: '16', value: '16' },
+          { label: '24', value: '24' },
+          { label: '48', value: '48' },
+        ]}
+        defaultValue="16"
+        className="w-[136px] xl:w-[128px]"
+      />
+    </>
+  );
+};
