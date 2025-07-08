c382615bcf0173107c2e9d136157719cd693c3f8
lidiia-tsymborovych <lidacimbirovic@gmail.com>
2025-07-08 18:16:38 +0300

    feat(footer): implement footer with logo, navigation links, and back-to-top button

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Outlet } from 'react-router-dom';
 import { Header } from './components/sections/Header/Header';
+import { Footer } from './components/sections/Footer/Footer';
 export const App = () => {
   return (
-    <div>
+    <div className="min-h-screen flex flex-col">
       <Header />
-      <div className="section">
+      <div className="section flex-grow">
         <div className="container">
           <Outlet />
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
