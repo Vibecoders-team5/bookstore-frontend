bf57c3ae0331bece04d4b40a2940c37d88516d27
Yura <yurii.st11@gmail.com>
2025-07-10 02:01:47 +0300

    add fav and not found placeholder

--- src/Pages/NotFoundPage/NotFoundPage.tsx
+++ src/Pages/NotFoundPage/NotFoundPage.tsx
@@ -1,7 +1,16 @@
 export const NotFoundPage = () => {
   return (
-    <div className="flex justify-center">
-      <img src="/books/img/page-not-found.png" alt="Page not found" />
+    <div className="flex flex-col items-center justify-center text-center gap-4 py-20">
+      <img
+        src="/books/img/page-not-found.png"
+        alt="Page not found"
+        className="w-100"
+      />
+      <p className="text-[#89939A] text-base sm:text-lg">
+        Oops something went wrong...
+        <br />
+        Try another page
+      </p>
     </div>
   );
 };
