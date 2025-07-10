ac46da894c538eda62a379280b14ebc28361e2a4
Yura <yurii.st11@gmail.com>
2025-07-10 14:59:37 +0300

    fix refreshed placeholder for dropdown

--- src/components/sections/CatalogTemplate/DropdownSortBy.tsx
+++ src/components/sections/CatalogTemplate/DropdownSortBy.tsx
@@ -5,6 +5,8 @@ export const DropdownSortBy = () => {
   const [searchParams] = useSearchParams();
   const navigate = useNavigate();
 
+  const currentSort = searchParams.get('sort');
+
   const sortOptions: Option[] = [
     { label: 'Newest', value: 'newest' },
     { label: 'Alphabetically', value: 'alphabetically' },
@@ -17,11 +19,12 @@ export const DropdownSortBy = () => {
     params.set('page', '1');
     navigate(`?${params.toString()}`);
   };
+
   return (
     <DropdownSelect
       options={sortOptions}
       placeholder="Newest"
-      defaultValue="newest"
+      defaultValue={currentSort ?? 'newest'}
       label="Sort by"
       className="w-[176px] h-15"
       onChange={handleSortChange}
