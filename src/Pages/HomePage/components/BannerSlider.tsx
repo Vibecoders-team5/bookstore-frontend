f98f11aa97c9e6849d26fa55623fe7a06e08bc72
Veronika Okhten <veronika.oxt12@gmail.com>
2025-07-11 16:56:43 +0300

    all banners added

--- src/Pages/HomePage/components/BannerSlider.tsx
+++ src/Pages/HomePage/components/BannerSlider.tsx
@@ -3,11 +3,13 @@ import { motion } from 'framer-motion';
 import { ChevronLeft, ChevronRight } from 'lucide-react';
 import { Button } from '@/components/ui/button';
 
-import ConstitutionDayDesktop from '/books/img/testbanner1.png';
+import ConstitutionDayDesktop from 'public/books/img/tetsbanner1.png';
 import ConstitutionDayTablet from '/books/img/testbannerTablet1.png';
 
 import ConstitutionDesktop from '/books/img/testbanner22.png';
 import ConstitutionTablet from '/books/img/testbannerTablet2.png';
+
+import ConstitutionDesktop3 from '/books/img/testbanner3.png';
 
 const images = [
   {
@@ -17,6 +19,9 @@ const images = [
   {
     desktop: ConstitutionDesktop,
     tablet: ConstitutionTablet,
+  },
+  {
+    desktop: ConstitutionDesktop3,
   },
 ];
 
