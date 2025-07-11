c9d71de5cee92a9cb845cb93b527ba23c8d3ec51
lidiia-tsymborovych <lidacimbirovic@gmail.com>
2025-07-11 14:44:05 +0300

    fix: make bookCard responsive

--- src/components/bloks/BookCard/BookCard.tsx
+++ src/components/bloks/BookCard/BookCard.tsx
@@ -38,23 +38,27 @@ export const BookCard: React.FC<BookCardProps> = ({ book }) => {
   };
 
   return (
-    <div className="w-full h-full lg:max-w-[272px] relative flex flex-col p-8 gap-4 rounded-lg border-1 border-gray-200 hover:shadow-lg bg-white">
-      <HeadphonesRound />
-      <Link to={`/${book.type}/${book.slug}`}>
-        <div className="w-full h-80">
+    <div className="w-full h-full lg:max-w-[272px] flex flex-col p-8 gap-4 rounded-lg border-1 border-gray-200 hover:shadow-lg bg-white">
+      <Link to={`/${book.type}/${book.slug}`} className="flex justify-center">
+        <div className="relative w-52 h-66">
+          <HeadphonesRound />
           <img
-            className="w-full h-full object-contain"
+            className="w-full h-full object-contain rounded-md"
             src={`/books/${book.images[0]}`}
             alt={book.name}
           />
         </div>
       </Link>
-      <div className="flex flex-col gap-2">
-        <Link to={`/${book.type}/${book.slug}`}>
+
+      <div className="grid grid-cols-[1fr_auto] sm:grid-cols-1 gap-4 sm:gap-2 w-full">
+        <Link
+          to={`/${book.type}/${book.slug}`}
+          className="flex flex-col min-w-0"
+        >
           <div>
             <Tooltip>
               <TooltipTrigger asChild>
-                <h5 className="h5 truncate max-w-[200px]">{book.name}</h5>
+                <h5 className="h5 truncate ">{book.name}</h5>
               </TooltipTrigger>
               <TooltipContent className="bg-black text-white">
                 <p>{book.name}</p>
@@ -63,11 +67,12 @@ export const BookCard: React.FC<BookCardProps> = ({ book }) => {
             <p className="body-text">{book.author}</p>
           </div>
         </Link>
-        <div className="flex flex-col">
+
+        <div className="flex flex-col items-end sm:items-start flex-shrink-0">
           {book.priceDiscount ?
-            <div className="flex items-center gap-2">
-              <h3 className="h3">{`$${book.priceDiscount}`}</h3>
-              <h4 className="h4 line-through text-gray-400">{`$${book.priceRegular}`}</h4>
+            <div className="flex flex-col sm:flex-row gap-1 sm:gap-2 items-start sm:items-center">
+              <h3 className="text-[20px] sm:text-[22px] font-[700]">{`$${book.priceDiscount}`}</h3>
+              <h4 className="text-[16px] sm:text-[20px] font-[600] line-through text-gray-400">{`$${book.priceRegular}`}</h4>
             </div>
           : <h3 className="h3">{`$${book.priceRegular}`}</h3>}
           <div className="inline-flex gap-1.5">
@@ -76,11 +81,13 @@ export const BookCard: React.FC<BookCardProps> = ({ book }) => {
           </div>
         </div>
       </div>
-      <div className="inline-flex gap-2">
+
+      <div className="flex gap-2 w-full">
         <AddButton
           onClick={toggleAddToCart}
           isSelected={isBookInCart}
           size={'customAddButton'}
+          className="flex-1"
         />
         <HeartButton
           onClick={toggleAddToFavourites}
--- src/components/ui/Icons/HeadphonesRound.tsx
+++ src/components/ui/Icons/HeadphonesRound.tsx
@@ -1,7 +1,7 @@
 import { Headphones } from 'lucide-react';
 
 export const HeadphonesRound = () => (
-  <div className="absolute flex items-center justify-center bg-green-600 rounded-full size-10 top-8 right-6">
+  <div className="absolute flex items-center justify-center bg-green-600 rounded-full size-10 top-0 -right-2 z-10">
     <Headphones className="text-white stroke-2" />
   </div>
 );
