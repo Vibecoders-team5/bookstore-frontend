deb1ac5137271da78f64df5881e74f1e28af602b
Veronika Okhten <veronika.oxt12@gmail.com>
2025-07-15 17:36:42 +0300

    favicon + categories videos

--- src/App.tsx
+++ src/App.tsx
@@ -11,13 +11,16 @@ export const App = () => {
   }, [setTheme, theme]);
 
   return (
-    <div className="min-h-screen flex flex-col">
-      <Header />
-      <main className="section flex-grow">
-        {/* pt-12 xl:pt-16 deleted these classes */}
-        <Outlet />
-      </main>
-      <Footer />
-    </div>
+    <>
+      <link rel="icon" href="books/img/favicon.png" type="image/png" />
+      <div className="min-h-screen flex flex-col">
+        <Header />
+        <main className="section flex-grow">
+          {/* pt-12 xl:pt-16 deleted these classes */}
+          <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </>
   );
 };
--- src/Pages/HomePage/components/CategoriesGrid.tsx
+++ src/Pages/HomePage/components/CategoriesGrid.tsx
@@ -1,52 +1,55 @@
-import { useFetchBooksStore } from '@/store/useFetchBooksStore';
 import { useTranslation } from 'react-i18next';
-
-const Banner1 = '/books/img/categories/paperbookcategory.jpg';
-const Banner2 = '/books/img/categories/audiobookcategory.jpg';
-const Banner3 = '/books/img/categories/kindlebookcategory.jpg';
+import { useEffect, useState } from 'react';
+import { useCategories } from '@/data/categories';
 
 export const CategoriesGrid = () => {
-  const { paperBooks, kindleBooks, audioBooks } = useFetchBooksStore();
+  const categories = useCategories();
+  const [animationClass, setAnimationClass] = useState(
+    'opacity-0 -translate-x-full',
+  );
   const { t } = useTranslation();
 
-  const categories = [
-    {
-      image: Banner1,
-      link: '#/paperback',
-      title: t('paperBooks'),
-      subtitle: `${paperBooks.length} ${t('items')}`,
-    },
-    {
-      image: Banner2,
-      link: '#/audiobook',
-      title: t('audioBooks'),
-      subtitle: `${audioBooks.length} ${t('items')}`,
-    },
-    {
-      image: Banner3,
-      link: '#/kindle',
-      title: t('kindleBooks'),
-      subtitle: `${kindleBooks.length} ${t('items')}`,
-    },
-  ];
+  const handleScroll = () => {
+    const element = document.getElementById('categoriesGrid');
+    if (element) {
+      const rect = element.getBoundingClientRect();
+      if (rect.top < window.innerHeight && rect.bottom >= 0) {
+        setAnimationClass('opacity-100 translate-x-0');
+      } else {
+        setAnimationClass('opacity-0 -translate-x-full');
+      }
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+    handleScroll();
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
   return (
-    <div className="max-w-[1136px] flex flex-wrap mx-4 sm:mx-4 md:mx-6 lg:mx-8 xl:mx-auto mt-12">
-      <h1 className="font-bold text-[2rem] text-[#313237] dark:text-white mb-[23px]">
+    <div
+      id="categoriesGrid"
+      className={`max-w-[1136px] flex flex-wrap mx-4 sm:mx-4 md:mx-6 lg:mx-8 xl:mx-auto mt-12 transition-all duration-[3.5s] ease-in-out ${animationClass}`}
+    >
+      <h1 className="font-bold text-[2rem] text-[#313237] dark:text-white mb-6">
         {t('shopByCat')}
       </h1>
       <div className="grid grid-cols-1 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4 w-full">
-        {categories.map(({ image, link, title, subtitle }) => (
+        {categories.map(({ video, link, title, subtitle }) => (
           <a
             key={title}
             href={link}
             className="block w-full overflow-hidden group"
           >
             <div className="aspect-square sm:aspect-[368/289] overflow-hidden rounded-lg">
-              <img
-                src={image}
-                alt={title}
+              <video
+                src={video}
+                title={title}
                 className="w-full h-full object-cover transition-transform duration-300 group-hover:scale-105"
+                autoPlay
+                loop
+                muted
               />
             </div>
 
