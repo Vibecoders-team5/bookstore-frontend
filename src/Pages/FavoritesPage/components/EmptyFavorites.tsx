0c8f3287d0b2f42bf00707cb8e6fe1f6d6e38b16
anastasiia <anastasiia.zuieva4@gmail.com>
2025-07-15 15:20:25 +0300

    anastasiia-78/home & favorites

--- src/Pages/FavoritesPage/components/EmptyFavorites.tsx
+++ src/Pages/FavoritesPage/components/EmptyFavorites.tsx
@@ -2,10 +2,10 @@ import { Trans } from 'react-i18next';
 
 export const EmptyFavorites = () => {
   return (
-    <div className="flex flex-col items-center justify-center text-center gap-4 py-20">
+    <div className="flex flex-col items-center justify-center gap-4 py-20 text-center">
       <img
         src="/books/img/favorites-is-empty.png"
-        alt="Empty favorites illustration"
+        alt="Favorites empty"
         className="w-48 h-auto"
       />
 
--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,14 +1,16 @@
+import { useEffect, useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
+
+import { useFetchBooksStore } from '@/store/useFetchBooksStore';
+import { getNewestBooks } from '@/utils/getNewestBooks';
+import { getRandomBooks } from '@/utils/getRandomBooks';
+
 import BannerSlider from '@/Pages/HomePage/components/BannerSlider';
 import { PaperBookSlider } from '@/components/sections/BooksSliders/PaperBookSlider';
 import { CategoriesGrid } from '@/Pages/HomePage/components/CategoriesGrid';
 import { ScrollSection } from './components/ScrollSection';
-import { useEffect } from 'react';
-import { BookLoader } from '@/components/ui/BookLoader/BookLoader';
-import { getNewestBooks } from '@/utils/getNewestBooks';
-import { getRandomBooks } from '@/utils/getRandomBooks';
 import { MovingRows } from './components/MovingRows';
-import { useFetchBooksStore } from '@/store/useFetchBooksStore';
-import { useTranslation } from 'react-i18next';
+import { BookLoader } from '@/components/ui/BookLoader/BookLoader';
 
 export const HomePage = () => {
   const { allBooks, isLoading, fetchAllBooks } = useFetchBooksStore();
@@ -18,24 +20,22 @@ export const HomePage = () => {
     fetchAllBooks();
   }, [fetchAllBooks]);
 
+  const newestBooks = useMemo(() => getNewestBooks(allBooks), [allBooks]);
+  const randomBooks = useMemo(() => getRandomBooks(allBooks), [allBooks]);
+
   if (isLoading) {
     return <BookLoader />;
   }
+
   return (
     <>
       <ScrollSection />
       <section className="relative z-[2]">
         <BannerSlider />
-        <PaperBookSlider
-          books={getNewestBooks(allBooks)}
-          title={t('newBooks')}
-        />
+        <PaperBookSlider books={newestBooks} title={t('newBooks')} />
         <MovingRows />
         <CategoriesGrid />
-        <PaperBookSlider
-          books={getRandomBooks(allBooks)}
-          title={t('UMayLike')}
-        />
+        <PaperBookSlider books={randomBooks} title={t('UMayLike')} />
       </section>
     </>
   );
